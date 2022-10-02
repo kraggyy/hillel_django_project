6795546f95210c041b6f787114d06b87e99f3fcd
daniel <den.tymoshenko777@gmail.com>
2022-10-02 09:41:30 +0300

    items app + flake8

--- items/views.py
+++ items/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def items(request, *args, **kwargs):
     return render(request, 'items/index_html.html')
-# Create your views here.
+# Create your views here .
