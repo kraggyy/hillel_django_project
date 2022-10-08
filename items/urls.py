b1bedeac3d3a7274971e2d9ecff026049b96c9d1
daniel <den.tymoshenko777@gmail.com>
2022-10-08 17:44:56 +0300

    commit for 8 hw

--- items/urls.py
+++ items/urls.py
@@ -1,14 +1,7 @@
-from django.conf import settings
 from django.urls import path
-from items import views
+
+from items.views import items
 
 urlpatterns = [
-    path('', views.general, name='general'),
-    path('products/', views.products, name='products'),
+    path('items/', items, name='items'),
 ]
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # noqa
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # noqa
