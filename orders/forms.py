7005d8f91dfcfb302f36db027dfee79b2fd77ca6
daniel <den.tymoshenko777@gmail.com>
2022-11-06 13:47:28 +0200

    template update

--- orders/forms.py
+++ orders/forms.py
@@ -1,42 +1,53 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
+from orders.models import Discount
 from products.models import Product
 
 
-class UpdateCartOrderForm(forms.Form):
-    product = forms.UUIDField(required=True)
-
+class BaseShoppingCartForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
-        self.instance = kwargs['instance']
-
-    def clean_product_id(self):
-        try:
-            product = Product.objects.get(id=self.cleaned_data['product'])
-        except Product.DoesNotExist:
-            raise ValidationError('Wrong product id.')
-        return product
-
-    def save(self, action):
-        getattr(self.instance.products, action)(self.cleaned_data['product'])
+        self.order = kwargs['order']
 
 
-class RecalculateCartForm(forms.Form):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, *kwargs)
-        self.instance = kwargs['instance']
-        self.fields = {k: forms.IntegerField() if k.startswith(
-            'quantity') else forms.UUIDField() for k in self.data.keys() if
-                       k != 'csrfmiddlewaretoken'}
+class AddShoppingCartForm(BaseShoppingCartForm):
+    product_uuid = forms.UUIDField(
+        required=True
+    )
 
     def save(self):
+        product = Product.objects.get(id=self.cleaned_data['product_uuid'])
+        self.order.products.add(product)
+        return self.order.save()
 
-        for k in self.cleaned_data.keys():
-            if k.startswith('product_'):
-                index = k.split('_')[-1]
-                self.instance.products.through.objects \
-                    .filter(product_id=self.cleaned_data[f'product_{index}']) \
-                    .update(quantity=self.cleaned_data[f'quantity_{index}'])
-        return self.instance
+
+class DeleteShoppingCartForm(BaseShoppingCartForm):
+    product_uuid = forms.UUIDField(
+        required=True
+    )
+
+    def save(self):
+        product = Product.objects.get(id=self.cleaned_data['product_uuid'])
+        self.order.products.remove(product)
+        return self.order.save()
+
+
+class DiscountShoppingCartForm(BaseShoppingCartForm):
+    code = forms.CharField(max_length=30)
+
+    def is_valid(self):
+        if not self.errors:
+            try:
+                discount = Discount.objects.get(code=self.cleaned_data['code'])
+                if not discount.is_active:
+                    raise Discount.DoesNotExist
+            except Discount.DoesNotExist:
+                self.errors.update({
+                    'discount_error': 'Please write valid discount'
+                })
+        return super().is_valid()
+
+    def save(self):
+        discount = Discount.objects.get(code=self.cleaned_data['code'])
+        self.order.discount = discount
+        return self.order.save()
--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from orders.views import CartView, UpdateCartView, RecalculateCartView
+from orders.views import AddShoppingCartView, ShoppingCartView, \
+    DeleteShoppingCartView, ClearShoppingCartView, DiscountShoppingCartView, \
+    PayShoppingCartView, successful_pay
 
 urlpatterns = [
-    path('cart/', CartView.as_view(), name='cart'),
-    re_path(r'cart/(?P<action>add|remove)/',
-            UpdateCartView.as_view(),
-            name='update_cart'),
-    path('recalculate/', RecalculateCartView.as_view(),
-         name='recalculate_cart'),
+    path('shopping_cart/', ShoppingCartView.as_view(), name='shopping_cart'),
+    path('add/', AddShoppingCartView.as_view(), name='add_shopping_cart'),
+    path('del/', DeleteShoppingCartView.as_view(), name='delete_shopping_cart'), # noqa
+    path('clear/', ClearShoppingCartView.as_view(), name='clear_shopping_cart'), # noqa
+    path('discount/', DiscountShoppingCartView.as_view(), name='discount_shopping_cart'),  # noqa
+    path('pay/', PayShoppingCartView.as_view(), name='pay_shopping_cart'),
+    path('successful_pay/', successful_pay, name='successful_pay'),
 ]
